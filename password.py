a8bdea0c3e254855915ef23a4ce46abdb11eb621
zeylei <zeyu-lei@uiowa.edu>
2018-04-21 19:01:58 -0500

    b

--- password.py
+++ password.py
@@ -1,12 +1,13 @@
 import subprocess
 import os.path
-
+import sys
 
 prompt = "config/promptWindow.txt"
-warning = "config/warningWindow.txt"
 
 def getStr(p):
 	input = str(p.communicate()[0]).strip()
+	if sys.version_info[0] == "2":
+		return input
 	return input[2:len(input)-3]
 
 def verify(pw):
